c5424d99879234adb1fafa40794ffa9a348d0bd3
Dimitris Tsirbas <manisfatsa@gmail.com>
2022-12-20 17:14:09 +0200

    Build dynamic index

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,37 @@
 // Put your client side JS code here
-console.log('Hello world')
+
+const CORS_PROXY_URL = "http://127.0.0.1:5000/"
+const SHOP_API_URL = "https://wiki-shop.onrender.com"
+
+const TEMPLATES = {}
+
+function getCategories() {
+    fetch(CORS_PROXY_URL + "/" + SHOP_API_URL + "/categories", { method: "GET" })
+    .then(res => {
+        if(!res.ok) {
+            onError(res.status + res.statusText)
+        } else {
+            res.json().then(categories => {
+                displayCategories({categories: categories})
+            })
+        }
+    })
+
+}
+
+function displayCategories(categories) {
+    console.log(categories);
+    const categoriesContainer = document.getElementById("categories-container")
+    template = document.getElementById("categories-template").textContent
+    compiledTemplate = Handlebars.compile(template)
+    categoriesContainer.innerHTML = compiledTemplate(categories)
+    console.log("template printed");
+    console.log(compiledTemplate(categories));
+}
+
+function onError(errorMessage) {
+    console.log(errorMessage);
+}
+
+getCategories();
+
