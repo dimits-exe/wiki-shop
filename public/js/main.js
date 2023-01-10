2c5534123a6240f03f8552658e19f137512d39ab
Dimitris Tsirbas <manisfatsa@gmail.com>
2023-01-10 20:15:15 +0200

    Refactor user information to a class

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,12 @@
 "use strict"
 
 import { Store } from "./modules/store.mjs"
-import { displayTemplate,  showLabel, hideLabel, checkValidity, swapPasswordType} from "./modules/ui.mjs"
+import { User } from "./modules/user.mjs"
+import { displayTemplate, showLabel, hideLabel, checkValidity, swapPasswordType } from "./modules/ui.mjs"
 
 const HOST_URL = "http://localhost:8080"
 const CORS_PROXY_URL = "http://127.0.0.1:5000/"
 const SHOP_API_URL = "https://wiki-shop.onrender.com"
-const STORAGE_ID = "sessionId"
 
 const CATEGORY_TEMPLATE = document.getElementById("categories-template")
 const CATEGORY_CONTAINER = document.getElementById("categories-container")
@@ -16,7 +16,7 @@ const STORE_CONTAINER = document.getElementById("store-container")
 const MENU_CONTAINER = document.getElementById("subcategories-menu")
 const MENU_TEMPLATE = document.getElementById("subcategories-menu-template")
 
-const loginSuccess =  document.getElementById("success-label")
+const loginSuccess = document.getElementById("success-label")
 const loginUsernameLabel = document.getElementById("username-label")
 const loginForm = document.getElementById("login-form")
 const loginNameField = document.getElementById("login-username")
@@ -27,7 +27,7 @@ const loginShowPassButton = document.getElementById("login-show-pass")
 const spinner = document.getElementById("login-loading")
 
 let store = null
-let username = null
+let user = new User()
 
 initializePage()
 
@@ -63,7 +63,7 @@ async function initializePage() {
         displayTemplate(MENU_TEMPLATE.textContent, { subcategories: subcategories }, MENU_CONTAINER)
         const SELECT_SUBCATEGORY_BUTTON = document.getElementById("select-subcategory-button")
         SELECT_SUBCATEGORY_BUTTON.onclick = () => displayCategory()
-        
+
         // build page with all subcategories
         displayCategory()
     }
@@ -93,7 +93,7 @@ function displayCategory() {
 function addPurchaseHandlers() {
     const buttons = document.getElementsByClassName("purchase-button")
 
-    for(let button of buttons) {
+    for (let button of buttons) {
         button.onclick = async () => await addToCart(button.dataset.productId);
     }
 }
@@ -103,15 +103,19 @@ function addPurchaseHandlers() {
  * @param {string | int} productId product id
  */
 async function addToCart(productId) {
-    const product = store.getProductById(parseInt(productId));
-    
-    const res = await addToCartRequest(product)
-
-    if(!res.ok) {
-        const text = await res.text()
-        alert(text)
+    if (!user.isLoggedIn()) {
+        onError("Please log in your account to purchase products")
     } else {
-        refreshCart()
+        const product = store.getProductById(parseInt(productId));
+
+        const res = await addToCartRequest(product)
+    
+        if (!res.ok) {
+            const text = await res.text()
+            onError(text)
+        } else {
+            refreshCart()
+        }
     }
 }
 
@@ -122,7 +126,7 @@ async function addToCart(productId) {
  */
 function addToCartRequest(product) {
     const formData = {
-        username: username, sessionId: getSessionId(), product: product
+        username: user.username, sessionId: user.sessionId, product: product
     }
 
     return fetch(HOST_URL + "/cart/buy/", {
@@ -152,13 +156,13 @@ async function login() {
             showLabel(loginErrorLabel, "Error while logging-in: " + errorMsg)
         } else {
             let sessionId = await res.json()
-            username = loginNameField.value 
+            user.username = loginNameField.value
 
             console.log(sessionId)
-            saveSessionId(sessionId)
+            user.sessionId = sessionId
 
             hideLabel(loginErrorLabel)
-            showLabel(loginSuccess, "Welcome " + username)
+            showLabel(loginSuccess, "Welcome " + user.username)
         }
 
     }
@@ -178,25 +182,6 @@ function loginRequest() {
     })
 }
 
-/**
- * Save the current session ID.
- * @param {string} sessionId the newly acquired session ID
- */
-function saveSessionId(sessionId) {
-    window.sessionStorage.setItem(STORAGE_ID, sessionId);
-}
-
-/**
- * Get the last saved session ID.
- * @returns the last session ID
- */
-function getSessionId() {
-    return window.sessionStorage.getItem(STORAGE_ID);
-}
-
-/**
- * Delete the last session ID.
- */
-function resetSessionId() {
-    window.sessionStorage.removeItem(STORAGE_ID);
+function onError(errorMessage) {
+    alert(errorMessage)
 }
---
+++ public/js/modules/user.mjs
@@ -0,0 +1,46 @@
+/**
+ * A class that keeps track of the user's details in relation to the server.
+ * @dimits-exe
+ */
+export class User {
+    #username
+    #sessionId
+
+    /**
+     * Constructs a new user instance.
+     * @param {string | null} username the user's name, null if not logged in
+     */
+    constructor(username = null) {
+        this.#username = username
+        this.#sessionId = null
+    }
+
+    /**
+     * Check whether the user has logged in. This method does NOT check whether the login
+     * information is valid.
+     * @returns true if the user has logged in
+     */
+    isLoggedIn() {
+        this.#username !== null
+    }
+
+    get username() {
+        if (this.#username === null)
+            throw new Error("Username was never set")
+        else
+            return this.#username === null
+    }
+
+    get sessionId() {
+        if (this.#sessionId === null)
+            throw new Error("Session ID was never set")
+        else
+            return this.#sessionId
+    }
+
+    /**
+     * Save the current session ID.
+     * @param {string} sessionId the newly acquired session ID
+     */
+    set sessionId(sessionId) { this.#sessionId = sessionId; }
+}
